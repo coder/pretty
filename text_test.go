006dee5563fb753d9567a53ede6334aa3777f72d
Ammar Bandukwala <ammar@ammar.io>
2023-09-06 18:39:45 -0500

    Add LineWrap

--- text_test.go
+++ text_test.go
@@ -68,6 +68,62 @@ func TestFgColor(t *testing.T) {
 	t.Logf("txt: %s", txt)
 }
 
+func TestLineWrap(t *testing.T) {
+	t.Parallel()
+
+	testCases := []struct {
+		name     string
+		input    string
+		width    int
+		expected string
+	}{
+		{
+			name:     "None",
+			input:    "The crazy fox jumped",
+			width:    100,
+			expected: "The crazy fox jumped",
+		},
+		{
+			name:     "Basic",
+			input:    "The crazy fox jumped",
+			width:    10,
+			expected: "The crazy\nfox jumped",
+		},
+		{
+			name:     "WordBoundary",
+			input:    "The crazy_fox_jumped",
+			width:    10,
+			expected: "The\ncrazy_fox_jumped",
+		},
+		{
+			name:     "MultiLine",
+			input:    "aabb cc dd ee ff",
+			width:    4,
+			expected: "aabb\ncc\ndd\nee\nff",
+		},
+		{
+			name:     "EmptyString",
+			input:    "",
+			width:    10,
+			expected: "",
+		},
+		{
+			name:     "SingleWordLongerThanWrap",
+			input:    "supercalifragilisticexpialidocious",
+			width:    10,
+			expected: "supercalifragilisticexpialidocious",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			txt := String(tc.input)
+			LineWrap(tc.width).Format(txt)
+			requireText(t, txt, tc.expected)
+		})
+	}
+}
+
 func TestStyle(t *testing.T) {
 	errorStyle := Style{
 		FgColor(termenv.RGBColor("#ff0000")),
